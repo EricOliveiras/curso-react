19939e271f84d737cda635cd82e9738f3f9b15c5
EricOlv <88443797+EricOliveiras@users.noreply.github.com>
2021-09-01 18:38:11 -0300

    Continuando

---
+++ src/components/TaskDetails.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import Button from './Button';
+import './TaskDetails.css';
+
+const TaskDetails = () => {
+    const params = useParams();
+
+    return (
+        <>
+            <div className="back-button-container">
+                <Button>Voltar</Button>
+            </div>
+            <div className="task-details-container">
+                <h2>{params.taskTitle}</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, impedit a rerum natus tempora maiores.</p>
+            </div>
+        </>
+     );
+}
+ 
+export default TaskDetails;
